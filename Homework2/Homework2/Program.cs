b21913f4e279bd0a86629443114cc8353092d1f5
marijapoposka <marijapoposka55@gmail.com>
2026-03-21 20:50:27 +0100

    hw2 class03

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Sum Of Even Numbers");
+
+int[] numbers = new int[6];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"Enter integer no.{i + 1}:");
+
+    while (!int.TryParse(Console.ReadLine(), out numbers[i]))
+    {
+        Console.WriteLine("Invalid input! Enter a valid integer:");
+    }
+}
+
+int sum = 0;
+
+foreach (int num in numbers)
+{
+    if (num % 2 == 0)
+    {
+        sum += num;
+    }
+}
+
+Console.WriteLine($"The result is: {sum}");
---
+++ Homework2/StudentGroup-hw2-pt2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Student Groups");
+
+
+string[] studentsG1 = { "Anisija", "Miki", "Mira", "Ivana", "Trajko" };
+string[] studentsG2 = { "Kiki", "Marija", "Elena", "Ace", "Sara" };
+
+
+int groupNumber;
+Console.WriteLine("Enter student group (1 or 2):");
+
+while (!int.TryParse(Console.ReadLine(), out groupNumber) || (groupNumber != 1 && groupNumber != 2))
+{
+    Console.WriteLine("Invalid input! Please enter 1 or 2:");
+}
+
+
+if (groupNumber == 1)
+{
+    Console.WriteLine("The Students in G1 are:");
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("The Students in G2 are:");
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
