a5fa39ba84470c440d3956ba34bbc24fd90e3225
marijapoposka <marijapoposka55@gmail.com>
2026-04-05 01:16:45 +0200

    hw7 pt1

---
+++ Homework7-Class08-pt2-LINQs/hw7-pt1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+Queue<int> numbersQueue = new Queue<int>();
+
+string choice = "Y";
+
+while (choice.ToUpper() == "Y")
+{
+    Console.Write("Enter a number: ");
+    int number = int.Parse(Console.ReadLine());
+
+    numbersQueue.Enqueue(number);
+
+    Console.Write("Do you want to enter another number? (Y/N): ");
+    choice = Console.ReadLine();
+}
+
+Console.WriteLine("\nNumbers in the order you entered:");
+
+foreach (int num in numbersQueue)
+{
+    Console.WriteLine(num);
+}
